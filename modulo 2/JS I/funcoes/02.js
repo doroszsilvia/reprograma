8e3e66530d37cb0e68c06b0562a73b6d8d2b2120
Mariana Zangrossi <mari.zangue@protonmail.com>
2018-09-14 10:52:38 -0300

    Exercício corrigido

--- modulo 2/JS I/funcoes/02.js
+++ modulo 2/JS I/funcoes/02.js
@@ -8,7 +8,7 @@
 function frase(nome, pronome, objeto) {
     return `${nome} foi caminhar no parque. ${pronome} encontrou ${objeto}. ${nome} decidiu levar ${objeto} pra casa. \n`;
 }
-console.log(frase('Harry', 'Rony', 'Hermione'));
+console.log(frase('Harry', 'ele', 'varinha'));
 
 // Escreva uma função
 // em Javascript que recebe
@@ -16,7 +16,10 @@ console.log(frase('Harry', 'Rony', 'Hermione'));
 // a raiz quadrada dele.
 
 function raizQuadrada(numero) {
+    if (numero < 0) throw 'Não existe raiz quadrada de número negativo!' 
+    if (typeof numero !== 'number') throw 'Por favor, insira um número.'
     return Math.sqrt(numero);
+    // return num ** (1/2) ==> ** significa elevado a
 }
 console.log(`A raiz quadrada de 64 é ${raizQuadrada(64)} \n`);
 
@@ -26,27 +29,36 @@ console.log(`A raiz quadrada de 64 é ${raizQuadrada(64)} \n`);
 // número qualquer e retorna
 // a representação binaria dele.
 function toBinary(numero) {
+    if (numero !== Math.floor(numero)) throw 'Entre com um número inteiro.';
+    if (numero < 0) throw 'Entre com um número positivo';
+
     let binaryNumber = '';
-
-    while(numero > 1) {
-        binaryNumber += `${Math.floor(numero) % 2}`;
-        numero /= 2;
+    while(numero > 0) {
+        binaryNumber += `${numero % 2}`;
+        numero = Math.floor(numero / 2);
     }
-
     return binaryNumber.split('').reverse('').join('');
 
     //return numero.toString(2);
 }
-console.log(`O número 255 em binário é ${toBinary(25)} \n`);
+console.log(`O número 43 em binário é ${toBinary(43)} \n`);
 
+// result = 43 % 2 = 1; numero = 21.5
+// result = 21 % 2 = 1; numero = 10.75
+// result = 10 % 2 = 0; numero = 5.375
+// result = 5 % 2 = 1; 
 
 // Escreva uma função em Javascript
 // que recebe uma lista de números
 // e retorna o maior número da lista.
 
 function greatherNumber(arr) {
+    // typeof Array retorna function pq o Array é uma function (let x = new Array());
+    if (typeof arr !== 'object') throw 'Por favor, forneça uma lista'
+
     let greather = -Infinity;
     arr.forEach( item => {
+        if (typeof item !== 'number') throw 'Por favor, forneça uma lista apenas de números'
         if (item > greather) greather = item;
     });
     return greather;
@@ -60,8 +72,11 @@ console.log(`O maior número do array [2,5,32,7,9,14] é ${greatherNumber([2,5,3
 
 function isAngram() {
     let argLength = arguments.length;
+    if (argLength < 2) throw 'Você precisa fornecer pelo menos duas palavras' 
 
+    /* Validação do argumento */
     for (let i = 1; i < argLength; i++) {
+        if (typeof arguments[i] != 'string') throw 'Por favor, forneça uma palavra';
         if (arguments[i].length !== arguments[i - 1].length) return false;
     }
 

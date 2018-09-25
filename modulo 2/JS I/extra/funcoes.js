a87fecc5c95ea0ffc77fdf3fe5e4c2f6e563d973
Mariana <mari.zangue@protonmail.com>
2018-09-24 21:39:39 -0300

    Fazendo exercício de função

--- modulo 2/JS I/extra/funcoes.js
+++ modulo 2/JS I/extra/funcoes.js
@@ -5,34 +5,76 @@
 // use a funcao .toString().
 function decToHex(num) {
     let divisionResult = [];
+    const hexaElements = {
+        10: 'A',
+        11: 'B',
+        12: 'C',
+        13: 'D',
+        14: 'E',
+        15: 'F'
+    };
 
     do {
+        divisionResult.push( Math.floor(num % 16) );
         num = num / 16;
-        divisionResult.push( parseInt(num) );
     } 
     while(parseInt(num) >= 16)
-    console.log(divisionResult);
+    divisionResult.push( Math.floor(num) );
+
+    divisionResult = divisionResult.map( (item, index) => {
+        if (item >= 10) divisionResult[index] = hexaElements[item];
+        else divisionResult[index] = divisionResult[index];
+
+        return divisionResult[index];
+    });
+    
+    return divisionResult.reverse().join('');
+
 }
-
-decToHex(438);
-
 
 // Escreva uma funcao que 
 // recebe uma quantidade n
 // e retorna os n primeiros
 // numeros da sequencia de
 // Fibonacci.
-
-
-
+function fibonacciSequence(num) {
+    let sequence = [1, 1];
+    for(let i = 2; i < num; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence.join();
+}
 
 // Escreva uma funcao que
 // receve dois numeros e retorna
 // o minimo multiplo comum
 // entre eles.
+function mmc(num1, num2) {
+    let multiple = [2, 3, 5, 7];
+    let count = 0;
+    let mmcNumber = 1;
 
+    while ( (num1 !== 1 || num2 !== 1) ) {
 
-
+        if (num1 % multiple[count] === 0 && num2 % multiple[count] === 0) { 
+            num1 /= multiple[count];
+            num2 /= multiple[count];
+            mmcNumber *= multiple[count]; 
+        }
+        else if (num1 % multiple[count] === 0) { 
+            num1 /= multiple[count] 
+            mmcNumber *= multiple[count]; 
+        }
+        else if (num2 % multiple[count] === 0) { 
+            num2 /= multiple[count];
+            mmcNumber *= multiple[count];
+        }
+        else {
+            count++;
+        }
+    }
+    return mmcNumber;
+}
 
 // Escreva uma funcao que
 // recebe uma quantidade n
@@ -48,7 +90,9 @@ decToHex(438);
 //           121                                                                                                 
 //            1 
 
+function crazyPattern(num) {
 
+}
 
 
 // Escreva um funcao que
